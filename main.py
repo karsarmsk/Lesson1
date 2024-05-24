8143d922d469e58b64e2a4f4ef212f94507c00ea
karsarmsk <karasev-s@internet.ru>
2024-05-24 12:20:41 +0300

    Add files via upload
    Python введение

---
+++ main.py
@@ -0,0 +1,23 @@
+# Задача «Длина слова».
+a = 'Длина слова'
+print(len(a))
+# Задача «Суммы и разности».
+first = 35
+second = 25
+summa = first + second
+diff = first - second
+print(summa, diff)
+#Задача «Среднее арифметическогое».
+third = 45
+mean = (summa + third) / 3
+print(mean)
+#Задача «Простые строчки».
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+# Задача «Сложная формула».
+a = 1
+b = 2
+c = 3
+f = (((a * b) + (a * c)) ** 3) / 2
+print(f)
